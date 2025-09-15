1bc85c1f11c8ce1c0cd61d8ee500d10be4b2ee4d
PTHARRISH <harrishpt@gmail.com>
2025-09-15 11:42:36 +0530

    PIP to UV Migration

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,10 +1,11 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
 
 # Custom User model with additional fields like phone number, DOB, etc.
 class CustomUser(AbstractUser):
-    user_role =  models.CharField(max_length=15, null=True, blank=True)
-    email =  models.CharField(max_length=15, null=True, blank=True)
+    user_role = models.CharField(max_length=15, null=True, blank=True)
+    email = models.CharField(max_length=15, null=True, blank=True)
     phone_number = models.CharField(max_length=15, null=True, blank=True)
 
     def __str__(self):
--- auth_app/serializers.py
+++ auth_app/serializers.py
@@ -1,16 +1,18 @@
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
 from .models import CustomUser
+
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['username', 'phone_number', 'email']  # Add or remove fields as needed
+        fields = ["username", "phone_number", "email"]  # Add or remove fields as needed
 
 
 class LoginTokenObtain(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super(LoginTokenObtain, cls).get_token(user)
-        token['username'] = user.username
+        token["username"] = user.username
         return token
